0876e7bb19bced07ad6c3387e81ff4a81fce306e
Sabra Ossen <sabraosn@gmail.com>
2017-12-14 04:01:48 -0300

    mofified code

--- rnn/rnn_stft_ip.m
+++ rnn/rnn_stft_ip.m
@@ -6,57 +6,76 @@ upperFr=30;
 data = load_data('k3b',0.4, 1, 0);
 xtr = data.Xtr;
 ytr = data.Ytr;
-yc = categorical(ytr);
-
 xte = data.Xte;
 yte = data.Yte;
 
-%Mu = 3-7, lowerFr=6Hz upperFr=14Hz,  Mu+Beta = 3-15,lowerFr=6Hz upperFr=30Hz
-%Only set numbers which are divisible by 2
-str = abs(dostft(xtr, 128, 64, 'hann', lowerFr, upperFr));
-ste = abs(dostft(xte, 128, 64, 'hann', lowerFr, upperFr));
+k3b_acc = STFT_RNN(xtr, ytr, xte, yte, lowerFr, upperFr, 630);
 
-tr_trails = size(xtr,3);
-sTr = str';
-xc = cell(tr_trails,1); % Cell vector of 3 channels each with 1000 frames
-for i = 1:tr_trails
-    xc(i) = {reshape(sTr(i,:),[630, 1])};
+data = load_data('k6b',0.6, 1, 0);
+xtr = data.Xtr;
+ytr = data.Ytr;
+xte = data.Xte;
+yte = data.Yte;
+
+k6b_acc = STFT_RNN(xtr, ytr, xte, yte, lowerFr, upperFr, 630);
+
+data = load_data('l1b',0.4, 1, 0);
+xtr = data.Xtr;
+ytr = data.Ytr;
+xte = data.Xte;
+yte = data.Yte;
+
+l1b_acc = STFT_RNN(xtr, ytr, xte, yte, lowerFr, upperFr, 630);
+
+disp(k3b_acc);
+disp(k6b_acc);
+disp(l1b_acc);
+
+function [acc] = STFT_RNN(xtr, ytr, xte, yte, lowerFr, upperFr, elem)
+    str = abs(dostft_vector(xtr, 128, 64, 'hann', lowerFr, upperFr));
+    ste = abs(dostft_vector(xte, 128, 64, 'hann', lowerFr, upperFr));
+
+    tr_trails = size(xtr,3);
+    sTr = str';
+    xc = cell(tr_trails,1); % Cell vector of 3 channels each with 1000 frames
+    for i = 1:tr_trails
+        xc(i) = {reshape(sTr(i,:),[elem, 1])};
+    end
+
+    inputSize = elem;
+    outputSize = 150;
+    outputMode = 'last';
+    numClasses = 4;
+    miniBatchSize = 50;
+
+    layers = [ ...
+        sequenceInputLayer(inputSize)
+        lstmLayer(outputSize,'OutputMode',outputMode)
+        lstmLayer(outputSize,'OutputMode',outputMode)
+        fullyConnectedLayer(numClasses)
+        softmaxLayer
+        classificationLayer];
+
+    maxEpochs = 1500;
+    shuffle = 'never';
+
+    options = trainingOptions('sgdm', ...
+        'MaxEpochs',maxEpochs, ...
+        'Shuffle', shuffle, ...
+        'MiniBatchSize',miniBatchSize, ...
+        'Plots','training-progress');
+    yc = categorical(ytr);
+    net = trainNetwork(xc,yc,layers,options);
+
+    te_trails = size(xte,3);
+    sTe = ste';
+    xtec = cell(te_trails,1);
+    for i = 1:te_trails
+        xtec(i) = {reshape(sTe(i,:),[elem, 1])};
+    end
+
+    Ytec = categorical(yte);
+    YPred = classify(net,xtec);
+
+    acc = (nnz(YPred == Ytec)/numel(Ytec))*100;
 end
-
-inputSize = 630;
-outputSize = 150;
-outputMode = 'last';
-numClasses = 4;
-miniBatchSize = 50;
-
-layers = [ ...
-    sequenceInputLayer(inputSize)
-    lstmLayer(outputSize,'OutputMode',outputMode)
-    lstmLayer(outputSize,'OutputMode',outputMode)
-    fullyConnectedLayer(numClasses)
-    softmaxLayer
-    classificationLayer];
-
-maxEpochs = 1500;
-shuffle = 'never';
-
-options = trainingOptions('sgdm', ...
-    'MaxEpochs',maxEpochs, ...
-    'Shuffle', shuffle, ...
-    'MiniBatchSize',miniBatchSize, ...
-    'Plots','training-progress');
-
-net = trainNetwork(xc,yc,layers,options);
-
-te_trails = size(xte,3);
-sTe = ste';
-xtec = cell(te_trails,1);
-for i = 1:te_trails
-    xtec(i) = {reshape(sTe(i,:),[630, 1])};
-end
-
-Ytec = categorical(yte);
-YPred = classify(net,xtec);
-
-acc = (nnz(YPred == Ytec)/numel(Ytec))*100;
-disp(acc);
--- svm/nmf_svm.m
+++ svm/nmf_svm.m
@@ -3,17 +3,44 @@ lowerFr=2;
 upperFr=30;
 iterations_1=500;
 iterations_2=500;
-basis_vecs=70;
 
 data = load_data('k3b',0.4, 1, 0);
 xtr = data.Xtr;
 ytr = data.Ytr;
 xte = data.Xte;
 yte = data.Yte;
+basis_vecs=70;
 
-[h_tr, h_te] = nmf(xtr, xte, basis_vecs, iterations_1, iterations_2, lowerFr, upperFr);
+k3b_acc = NMF_SVM_Classifier(xtr, ytr, xte, yte,iterations_1, iterations_2, lowerFr, upperFr, basis_vecs);
 
-ytepred = multisvm(h_tr',ytr,h_te');
+data = load_data('k6b',0.6, 1, 0);
+xtr = data.Xtr;
+ytr = data.Ytr;
+xte = data.Xte;
+yte = data.Yte;
+basis_vecs=30;
+iterations_1=1000;
+iterations_2=1000;
 
-acc = (nnz(ytepred == yte)/numel(yte))*100;
-disp(acc);
+k6b_acc = NMF_SVM_Classifier(xtr, ytr, xte, yte,iterations_1, iterations_2, lowerFr, upperFr, basis_vecs);
+
+data = load_data('l1b',0.4, 1, 0);
+xtr = data.Xtr;
+ytr = data.Ytr;
+xte = data.Xte;
+yte = data.Yte;
+basis_vecs=40;
+iterations_1=500;
+iterations_2=500;
+
+l1b_acc = NMF_SVM_Classifier(xtr, ytr, xte, yte,iterations_1, iterations_2, lowerFr, upperFr, basis_vecs);
+
+disp(k3b_acc);
+disp(k6b_acc);
+disp(l1b_acc);
+
+function [acc] = NMF_SVM_Classifier(xtr, ytr, xte, yte,iterations_1, iterations_2, lowerFr, upperFr, basis_vecs)
+    [h_tr, h_te] = nmf(xtr, xte, basis_vecs, iterations_1, iterations_2, lowerFr, upperFr);
+    ytepred = multisvm(h_tr',ytr,h_te');
+    acc = (nnz(ytepred == yte)/numel(yte))*100;
+end
